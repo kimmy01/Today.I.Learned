c484da5de42726ce2b6478463fb07a45d81bf7db
kimmy01 <nkim40181@hanmail.net>
2021-07-23 00:08:23 +0900

    chore : UserController로 옮겨간 부분 주석처리

--- Spring/jwt-tutorial/src/main/java/com/example/jwttutorial/controller/HelloController.java
+++ Spring/jwt-tutorial/src/main/java/com/example/jwttutorial/controller/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api")
-public class HelloController {
-    @GetMapping("/hello")
-    public ResponseEntity<String> hello(){
-        return ResponseEntity.ok("hello");
-    }
-}
+//@RestController
+//@RequestMapping("/api")
+//public class HelloController {
+//    @GetMapping("/hello")
+//    public ResponseEntity<String> hello(){
+//        return ResponseEntity.ok("hello");
+//    }
+//}
