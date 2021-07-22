ef442f38de1887e72bfaa47913c6e6db8417fa84
kimmy01 <nkim40181@hanmail.net>
2021-07-22 23:38:14 +0900

    feat : UserService 수정(import 수정) + 주석 추가

--- Spring/jwt-tutorial/src/main/java/com/example/jwttutorial/service/UserService.java
+++ Spring/jwt-tutorial/src/main/java/com/example/jwttutorial/service/UserService.java
@@ -4,7 +4,7 @@ import com.example.jwttutorial.dto.UserDto;
 import com.example.jwttutorial.entity.Authority;
 import com.example.jwttutorial.entity.User;
 import com.example.jwttutorial.repository.UserRepository;
-import org.apache.catalina.security.SecurityUtil;
+import com.example.jwttutorial.util.SecurityUtil;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,17 +20,17 @@ public class UserService {
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
-    }
+    } //UserRepository, PasswordEncoder 주입받음
 
     @Transactional
-    public User signup(UserDto userDto) {
+    public User signup(UserDto userDto) { //회원가입 로직 수행 메소드
         if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
             throw new RuntimeException("이미 가입되어 있는 유저입니다.");
-        }
+        } //이미 가입된 유저인지 찾아봄
 
         //빌더 패턴의 장점
         Authority authority = Authority.builder()
-                .authorityName("ROLE_USER")
+                .authorityName("ROLE_USER") //회원가입 통해서 생성되는 유저는 ROLE_USER 하나만 가짐
                 .build();
 
         User user = User.builder()
@@ -41,16 +41,16 @@ public class UserService {
                 .activated(true)
                 .build();
 
-        return userRepository.save(user);
+        return userRepository.save(user); //DB에 존재하지 않는 유저라면 Authority와 User 정보를 생성해서 UserRepository의 save 메소드를 이용해서 DB에 정보 저장
     }
 
     @Transactional(readOnly = true)
     public Optional<User> getUserWithAuthorities(String username) {
         return userRepository.findOneWithAuthoritiesByUsername(username);
-    }
+    } //username을 파라미터로 받아서 어떠한 username이든 username에 해당하는 user객체와 권한정보를 가져오는 메소드
 
     @Transactional(readOnly = true)
     public Optional<User> getMyUserWithAuthorities() {
         return SecurityUtil.getCurrentUsername().flatMap(userRepository::findOneWithAuthoritiesByUsername);
-    }
+    } //현재 SecurityContext에 저장된 username의 정보만 받아오는 메소드
 }
