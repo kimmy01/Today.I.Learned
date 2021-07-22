8bc93e78ca1c158ffe5e2954070f0c0a504b27a1
kimmy01 <nkim40181@hanmail.net>
2021-07-22 23:03:32 +0900

    chore : UserRepository 주석 추가

--- Spring/jwt-tutorial/src/main/java/com/example/jwttutorial/repository/UserRepository.java
+++ Spring/jwt-tutorial/src/main/java/com/example/jwttutorial/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    @EntityGraph(attributePaths = "authorities")
+    @EntityGraph(attributePaths = "authorities") //lazy조회 X, eager조회 O => authorities 정보 같이 가져옴
     Optional<User> findOneWithAuthoritiesByUsername(String username);
+    //username을 기준으로 User 정보를 가져올 때, 권한 정보도 같이 가져옴
 }
