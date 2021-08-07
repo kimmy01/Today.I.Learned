972ee9ee6006943f9f6472f5426b98490d24aa5d
kimmy01 <nkim40181@hanmail.net>
2021-08-07 23:15:10 +0900

    feat : 하위 컴포넌트에 state 값 주입하기(props)

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -9,7 +9,12 @@ class App extends Component {
     super(props);
     this.state = {
       subject:{title: "WEB", sub: "world wide web!"},
-      content:{title: "HTML", desc: "HTML is HyperText Markup Language"}
+      content:{title: "HTML", desc: "HTML is HyperText Markup Language"},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'},
+      ]
     }
   } //어떤 컴포넌트가 실행될 때, render보다 먼저 실행되면서 컴포넌트를 초기화 시켜줌
   render(){
@@ -19,7 +24,7 @@ class App extends Component {
           title = {this.state.subject.title} 
           sub = {this.state.subject.sub}>
         </Subject>
-        <TOC/>
+        <TOC data = {this.state.contents}></TOC>
         <Content 
           title = {this.state.content.title} 
           desc = {this.state.content.desc}>
