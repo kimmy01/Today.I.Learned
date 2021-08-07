a2c9731eaf4d504e7e5fa124e282c299acbdf7a8
kimmy01 <nkim40181@hanmail.net>
2021-08-07 23:28:07 +0900

    feat : 이벤트 발생에 따라서 state값 바꿔주기 1

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode: "welcome",
       subject:{title: "WEB", sub: "world wide web!"},
+      welcome:{title: "Welcome", desc: "Hello, React!"},
       content:{title: "HTML", desc: "HTML is HyperText Markup Language"},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
@@ -18,6 +20,15 @@ class App extends Component {
     }
   } //어떤 컴포넌트가 실행될 때, render보다 먼저 실행되면서 컴포넌트를 초기화 시켜줌
   render(){
+    console.log('App render');
+    var _title, _desc = null;
+    if(this.state.mode === "welcome"){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === "read"){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
@@ -26,8 +37,8 @@ class App extends Component {
         </Subject>
         <TOC data = {this.state.contents}></TOC>
         <Content 
-          title = {this.state.content.title} 
-          desc = {this.state.content.desc}>
+          title = {_title} 
+          desc = {_desc}>
         </Content>
       </div>
     );
