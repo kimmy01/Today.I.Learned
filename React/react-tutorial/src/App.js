c84a88be3297683cae7b954eb710ea999a3bc0fc
kimmy01 <nkim40181@hanmail.net>
2021-08-07 23:07:56 +0900

    feat : 하위컴포넌트에 state 값 적용하기

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -5,13 +5,25 @@ import Content from "./components/Content"
 import Subject from "./components/Subject"
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title: "WEB", sub: "world wide web!"},
+      content:{title: "HTML", desc: "HTML is HyperText Markup Language"}
+    }
+  } //어떤 컴포넌트가 실행될 때, render보다 먼저 실행되면서 컴포넌트를 초기화 시켜줌
   render(){
     return (
       <div className="App">
-        <Subject title = "WEB" sub = "world wide web!"></Subject>
-        <Subject title = "REACT" sub = "REACT!"></Subject>
+        <Subject 
+          title = {this.state.subject.title} 
+          sub = {this.state.subject.sub}>
+        </Subject>
         <TOC/>
-        <Content title = "HTML" desc = "HTML is HyperText Markup Language"></Content>
+        <Content 
+          title = {this.state.content.title} 
+          desc = {this.state.content.desc}>
+        </Content>
       </div>
     );
   }
