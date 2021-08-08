fe5e8f0b085e8ed701671a2725e2a16e41f2c7cb
kimmy01 <nkim40181@hanmail.net>
2021-08-08 22:46:37 +0900

    feat: Delete 기능 구현

--- React/react-tutorial/src/App.js
+++ React/react-tutorial/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     super(props);
     this.max_content_id = 3;
     this.state = {
-      mode: "create",
+      mode: "welcome",
       selected_content_id: 2,
       subject:{title: "WEB", sub: "world wide web!"},
       welcome:{title: "Welcome", desc: "Hello, React!"},
@@ -113,9 +113,28 @@ class App extends Component {
         }.bind(this)}
           data = {this.state.contents}></TOC>
         <Control onChangeMode={function(_mode){
-          this.setState({
-            mode: _mode
-          })
+          if(_mode === "delete"){
+            if(window.confirm()){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i + 1;
+              }
+              this.setState({
+                contents: _contents,
+                mode: "welcome"
+              });
+              alert('deleted!');
+            }
+          } else{
+            this.setState({
+              mode: _mode //읽기 모드 변경해주기
+            })
+          }
         }.bind(this)}/>
         {this.getContent()}
       </div>
